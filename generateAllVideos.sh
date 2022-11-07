529ab26d5927bd182dd8a3e440babcf466429631
kmsamuel <104738780+kmsamuel@users.noreply.github.com>
2022-11-06 21:46:09 -0800

    Delete generateAllVideos.sh

--- generateAllVideos.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-files= ls $1/*.min.tsv
-for eachfile in $files
-do
-   blender animation.blend -b -P createVideo.py -- $eachfile ./videos/iso_cam ISO_CAM
-   blender animation.blend -b -P createVideo.py -- $eachfile ./videos/rear_cam REAR_CAM
-   blender animation.blend -b -P createVideo.py -- $eachfile ./videos/side_cam SIDE_CAM
-done
